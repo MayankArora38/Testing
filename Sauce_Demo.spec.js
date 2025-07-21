b1d47ad756eb14f664636331a0a6c7c3cd3ffe28
Mayank Arora <mayankarora38@gmail.com>
2025-07-21 18:24:12 +0530

    Add files via upload

---
+++ Sauce_Demo.spec.js
@@ -0,0 +1,33 @@
+import { test, expect } from '@playwright/test';
+
+test('test', async ({ page }) => {
+  await page.goto('https://www.saucedemo.com/');
+  await page.locator('[data-test="username"]').click();
+  await page.locator('[data-test="username"]').fill('standard_user');
+  await page.locator('[data-test="password"]').click();
+  await page.locator('[data-test="password"]').fill('secret_sauce');
+  await page.locator('[data-test="login-button"]').click();
+  await page.goto('https://www.saucedemo.com/inventory.html');
+  await page.locator('[data-test="add-to-cart-sauce-labs-backpack"]').click();
+  await page.locator('[data-test="add-to-cart-sauce-labs-bolt-t-shirt"]').click();
+  await page.locator('[data-test="shopping-cart-link"]').click();
+  await page.locator('[data-test="cart-list"] div').filter({ hasText: '1Sauce Labs Backpackcarry.' }).locator('[data-test="item-quantity"]').click();
+  await page.locator('[data-test="cart-list"] div').filter({ hasText: '1Sauce Labs Bolt T-ShirtGet' }).locator('[data-test="item-quantity"]').click();
+  await page.locator('[data-test="checkout"]').click();
+  await page.locator('[data-test="firstName"]').click();
+  await page.locator('[data-test="firstName"]').fill('John');
+  await page.locator('[data-test="lastName"]').click();
+  await page.locator('[data-test="lastName"]').fill('Doe');
+  await page.locator('[data-test="postalCode"]').click();
+  await page.locator('[data-test="postalCode"]').fill('12345');
+  await page.locator('[data-test="continue"]').click();
+  await page.locator('[data-test="subtotal-label"]').click();
+  await page.locator('[data-test="tax-label"]').click();
+  await page.locator('[data-test="total-label"]').click();
+  await page.locator('[data-test="finish"]').click();
+  await page.locator('[data-test="checkout-complete-container"]').click();
+  await page.locator('[data-test="complete-header"]').click();
+  await page.locator('[data-test="back-to-products"]').click();
+  await page.getByRole('button', { name: 'Open Menu' }).click();
+  await page.locator('[data-test="logout-sidebar-link"]').click();
+});
